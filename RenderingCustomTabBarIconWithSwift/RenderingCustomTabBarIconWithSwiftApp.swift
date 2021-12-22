53e71e3b3272654092b3e69ffa424d49fba7f2b2
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-12-22 22:33:27 +0700

    Initial Commit

---
+++ RenderingCustomTabBarIconWithSwift/RenderingCustomTabBarIconWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenderingCustomTabBarIconWithSwiftApp.swift
+//  RenderingCustomTabBarIconWithSwift
+//
+//  Created by San Engineer on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RenderingCustomTabBarIconWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
